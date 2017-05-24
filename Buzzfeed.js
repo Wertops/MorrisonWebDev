cfed6122bc96fcf6da6915a68bc47bb59360a398
Wertops <carter17e@ncssm.edu>
2017-05-23 22:07:14 -0400

    IDK I need to start over
    idk??? Added the result finder stuff

--- Buzzfeed.js
+++ Buzzfeed.js
@@ -1,15 +1,14 @@
-function(addQuestion);
-{
- //document.createElement and then create a div
-  var newQuestion = document.createElement(question[]);
-}
-function(addPhoto);
-{
-  
-}
+
 function(makeResult);
 {
-  
+  var qstns;
+  //might have to change the two below tagnames to ids. I'm not exactly sure
+  var resultnumber = document.getElementbyTagName("resultno")
+  var questionnumber = document.getElementbyTageName("questionno")
+  // if you feel like it and have time, you can add the "add question button again. var numItems = $('.item').length; will make a variable to count the number of classes
+  for (qstns = 1; qstns <= questionno; qstns++){
+   //here I am trying to 
+   if (document.getElementByID(
 }
 function(makeAnother);
 {
